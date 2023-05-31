59ea6fe48ebf03ab8e8785c83aebb96393dd4725
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-05-31 14:55:35 +0600

    added notifications and order in progress

--- src/components/laundry-customer/notifications.tsx
+++ src/components/laundry-customer/notifications.tsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ActivePageType } from "../../utils/activePageTypes";
 import NavbarCustomer from "../partials/navbarCustomer";
 import HeaderCustomer from "../partials/headerCustomer";
 import { Pagination } from "react-bootstrap";
+import newRequest from "../../utils/newRequest";
+
+
+interface Notification {
+  notification_id: number;
+  message: string;
+  formattedTime: string;
+  status: string;
+}
 
 const CustomerNotifications = () => {
   const [navigation, setNavigation] = useState(false);
-  const [notifications] = useState([
-    { orderId: 1, timestamp: "11:00 PM", message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur turpis ac velit dapibus, at varius lorem tincidunt." },
-  { orderId: 2, timestamp: "11:00 PM", message: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer auctor erat sit amet mi porta dapibus." },
-  { orderId: 3, timestamp: "11:00 PM", message: "Suspendisse ac diam non justo aliquet lobortis eu a dolor. Aliquam ut venenatis massa, in laoreet tortor." },
-  { orderId: 4, timestamp: "11:00 PM", message: "Fusce dapibus gravida elit a tempus. Phasellus pretium sapien mi, et rhoncus justo auctor et." },
-  { orderId: 5, timestamp: "10:00 PM", message: "Duis maximus mi in quam pellentesque efficitur. Aenean auctor sem non purus rutrum, eu ullamcorper ipsum ultrices." },
-  { orderId: 6, timestamp: "11:00 PM", message: "Quisque eu felis ac justo tempor congue. Vivamus volutpat ipsum quis eros efficitur dapibus." },
-  { orderId: 7, timestamp: "2:00 PM", message: "Pellentesque non nulla id nisl consectetur auctor id sit amet risus. Donec mattis auctor finibus." },
-  { orderId: 8, timestamp: "11:00 PM", message: "Morbi lacinia neque in erat consectetur, vitae semper est elementum. Cras tincidunt pharetra mi, non pellentesque purus tristique eu." },
-  { orderId: 9, timestamp: "11:00 PM", message: "Sed dignissim aliquam finibus. Nam lobortis nunc id est rutrum, ac tempor metus varius." },
-  { orderId: 10, timestamp: "11:00 PM", message: "Nulla a nisi et ante posuere eleifend. Aenean semper leo at diam interdum, et lacinia enim dapibus." },
-  { orderId: 11, timestamp: "11:00 PM", message: "Vestibulum malesuada leo nec risus consectetur, a tempus dolor pulvinar. Sed pharetra pretium orci." },
-  { orderId: 12, timestamp: "11:00 PM", message: "Integer sit amet justo ut diam condimentum scelerisque vitae ac purus. Curabitur nec purus eu elit condimentum cursus et vitae dolor." },
-  ]);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  useEffect(() => {
+    const getTopNotifications = async () => {
+      await newRequest
+        .get("/notifications/all/nafisamaliyat@iut-dhaka.edu")
+        .then((res) => {
+          setNotifications(res.data);
+          // console.log(notifications);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    getTopNotifications();
+  }, []);
 
   const itemsPerPage = 4; // Number of items per page
   const totalPages = Math.ceil(notifications.length / itemsPerPage); // Total number of pages
@@ -45,16 +57,16 @@ const CustomerNotifications = () => {
           />
 
           <h1 className="notification-header">Your Notifications</h1>
-          {displayedNotifications.map((notification, index) => (
-            <div className="table view-reviews-table">
+          {displayedNotifications.map((notification) => (
+            <div className="table view-reviews-table" key={notification.notification_id}>
               <div className="notifications my-review">
                 <div className="container">
-                  <h3>Order ID: #{notification.orderId}</h3>
+                  <h3>Order ID: #</h3>
                   <span>
                     {notification.message}
                   </span>
                   <br />
-                  {notification.timestamp}
+                  {notification.formattedTime}
                 </div>
               </div>
             </div>
--- src/components/partials/headerManager.tsx
+++ src/components/partials/headerManager.tsx
@@ -1,18 +1,25 @@
 import { IonIcon } from "@ionic/react";
 import { menuOutline, notificationsOutline } from "ionicons/icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 type LaundryManager = {
   navigation: boolean;
   setNavigation: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const HeaderManager = ({
-  navigation,
-  setNavigation,
-}: LaundryManager) => {
+interface Notification {
+  notification_id: number;
+  message: string;
+  formattedTime: string;
+  read: boolean;
+}
+
+const HeaderManager = ({ navigation, setNavigation }: LaundryManager) => {
   const [notification, setNotification] = useState(false);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [unreadCount, setUnreadCount] = useState(0);
   const toggleNotification = () => {
     let box = document.getElementById("box");
     if (box && notification) {
@@ -27,6 +34,53 @@ const HeaderManager = ({
       setNotification(true);
     }
   };
+
+  const countUnreadNotifications = (notifications: Notification[]) => {
+    const unreadCount = notifications.reduce((count, notification) => {
+      if (!notification.read) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+    return unreadCount;
+  };
+
+  useEffect(() => {
+    const getTopNotifications = async () => {
+      await axios.get("http://localhost:8000/notifications/latest/nafisamaliyat@iut-dhaka.edu")
+        .then((res) => {
+          setNotifications(res.data);
+          const count = countUnreadNotifications(res.data);
+          setUnreadCount(count);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    getTopNotifications();
+  }, []);
+
+  const updateNotifications = async (currentNotifications: Notification[]) => {
+
+      await axios.post("http://localhost:8000/api/notifications/updateStatus",
+       { notifications: currentNotifications 
+      }).then((res) =>{
+        console.log(res);
+      }).catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleToggleNotification = async () => {
+    toggleNotification();
+    if (notifications !== null && notification && unreadCount!==0) {
+      // setTimeout(() => { setUnreadCount(0) }, 20);
+      setUnreadCount(0);
+      await updateNotifications(notifications);
+    }
+  };
+
   return (
     <div className="topbar">
       <div className="toggle">
@@ -37,50 +91,25 @@ const HeaderManager = ({
       </div>
 
       <div className="dropdowns">
-        <div className="notif_icon" onClick={toggleNotification}>
+        <div className="notif_icon" onClick={handleToggleNotification}>
           <div className="icon-container">
             <IonIcon icon={notificationsOutline}></IonIcon>
-            <span>17</span>
+            {unreadCount !== 0 && <span>{unreadCount}</span>}
           </div>
         </div>
         <div className="notifi-box" id="box">
           <h2>
-            Notifications <span>17</span>
+            Notifications {unreadCount !== 0 && <span>{unreadCount}</span>}
           </h2>
-          <div className="notifi-item">
-            <div className="text">
-              <span>Elias Abdurrahman</span>
-              <p>@lorem ipsum dolor sit amet</p>
-            </div>
-          </div>
 
-          <div className="notifi-item">
-            <div className="text">
-              <span>John Doe</span>
-              <p>@lorem ipsum dolor sit amet</p>
+          {notifications.map((notification) => (
+            <div className="notifi-item" key={notification.notification_id}>
+              <div className="text">
+                <h4>{notification.message}</h4>
+                <p>{notification.formattedTime}</p>
+              </div>
             </div>
-          </div>
-
-          <div className="notifi-item">
-            <div className="text">
-              <span>Emad Ali</span>
-              <p>@lorem ipsum dolor sit amet</p>
-            </div>
-          </div>
-
-          <div className="notifi-item">
-            <div className="text">
-              <span>Ekram Abu</span>
-              <p>@lorem ipsum dolor sit amet</p>
-            </div>
-          </div>
-
-          <div className="notifi-item">
-            <div className="text">
-              <span>Jane Doe LAST</span>
-              <p>@lorem ipsum dolor sit amet</p>
-            </div>
-          </div>
+          ))}
 
           <div className="notifi-item">
             <div className="text notif_see_more">
